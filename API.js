f33d2e88f03150e2472e13f83e3cec0aacbb1f41
STATH213 <81795351+STATH213@users.noreply.github.com>
2021-05-24 12:06:17 -0700

    Call Api
    - called api
    - get user lat,long
    - using fetch
    - updated html stuff
    - updated css

--- API.js
+++ API.js
@@ -1,14 +1,42 @@
 //const get_woeid = "https://www.metaweather.com/api/location/search/?query=adelaide";
 
-const api_url = "https://www.metaweather.com/api/location/1099805/?query=adelaide";
+const getLocationNameFromCoords = async function (position) {
+    const api_url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude}%2C${position.coords.longitude}&language=en`;
+    const response = await fetch(api_url);
+    const data = await response.json();
+    return data;
+}
 
-async function getapi(api_url) {
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+} 
+//Get the latitude and the longitude;
+async function successFunction(position) {
+    await getLocationNameFromCoords(position)
+}
+
+function errorFunction(){
+    alert("Geocoder failed");
+}
+
+const getWoeid = async function (location) {
+    const api_url = `https://www.metaweather.com/api/location/search/?query=${location}`;
+    const response = await fetch(api_url);
+    const data = await response.json();
+    return data[0].woeid;
+}
+
+export const getapi = async function (location) {
+    const woeid = await getWoeid(location);
+
+    const api_url = `https://www.metaweather.com/api/location/${woeid}/`;
+    //const api_url = "./weather_data.json";
     
     // Storing response
     const response = await fetch(api_url);
-    
+
     // Storing data in form of JSON
-    var data = await response.json();
-    console.log(data);
+    const data = await response.json();
+    return data;
 }
 
--- Data_to_html.js
+++ Data_to_html.js
@@ -1,121 +1,68 @@
-console.log("CONNECTED")
+import { getapi } from './API.js';
 
-images = {
-    "sn": "https://www.metaweather.com/static/img/weather/sn.svg",
-    "sl": "https://www.metaweather.com/static/img/weather/sl.svg",
-    "h": "https://www.metaweather.com/static/img/weather/h.svg",
-    "t": "https://www.metaweather.com/static/img/weather/t.svg",
-    "hr": "https://www.metaweather.com/static/img/weather/hr.svg",
-    "lr": "https://www.metaweather.com/static/img/weather/lr.svg",
-    "s": "https://www.metaweather.com/static/img/weather/s.svg",
-    "hc": "https://www.metaweather.com/static/img/weather/hc.svg",
-    "lc": "https://www.metaweather.com/static/img/weather/lc.svg",
-    "c": "https://www.metaweather.com/static/img/weather/c.svg"
+document.addEventListener('DOMContentLoaded', async function () {
+    console.log("CONNECTED")
+    console.log(getapi)
+    const weather = await getapi("sydney");
 
-};
+    const days = ["Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]; //day strings
 
-var dmon; var dtue; var dwed; var dthur; var dfri; var dsat; var dsun; //dataset vars
-var days = ["Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]; //day strings
-dayT = "Wednesday"; //todays day
+    console.log(weather);
+    weather.consolidated_weather.forEach((element, index) => {
+        console.log(element);
+        const date = new Date(element.applicable_date);
+        const dayIndex = date.getDay();
+        element.day = days[dayIndex];
+    });
 
-var pos = days.indexOf(dayT);
-var daycount = pos; //counting what day it is 
-maxdisplay = daycount + 3
-console.log(maxdisplay)
+    const images = {
+        "sn": "https://www.metaweather.com/static/img/weather/sn.svg",
+        "sl": "https://www.metaweather.com/static/img/weather/sl.svg",
+        "h": "https://www.metaweather.com/static/img/weather/h.svg",
+        "t": "https://www.metaweather.com/static/img/weather/t.svg",
+        "hr": "https://www.metaweather.com/static/img/weather/hr.svg",
+        "lr": "https://www.metaweather.com/static/img/weather/lr.svg",
+        "s": "https://www.metaweather.com/static/img/weather/s.svg",
+        "hc": "https://www.metaweather.com/static/img/weather/hc.svg",
+        "lc": "https://www.metaweather.com/static/img/weather/lc.svg",
+        "c": "https://www.metaweather.com/static/img/weather/c.svg"
 
-//dataset high/lowtemp - expec rain - expec wind
-var weather = [
-    {
-        "day": "Monday", 
-        "max_temp":23.075, 
-        "min_temp":17.15, 
-        "wind_speed":3.889568364018134, 
-        "the_temp":22.665,
-        "rain": 8.4,
-        "weather_state_abbr":"hc"
-    },
+    };
 
-    {
-        "day":"Tuesday",
-        "max_temp":33.075, 
-        "min_temp":24.15, 
-        "wind_speed":19.889568364018134, 
-        "the_temp":27.5,
-        "rain": 6,
-        "weather_state_abbr":"c"
-    },
+    var dmon; var dtue; var dwed; var dthur; var dfri; var dsat; var dsun; //dataset vars
+    const dayT = "Monday"; //todays day
 
-    {
-        "day": "Wednesday",
-        "max_temp":23.075, 
-        "min_temp":13.15, 
-        "wind_speed":9.889568364018134, 
-        "the_temp":21.665,
-        "rain": 5,
-        "weather_state_abbr":"c"
-    },
+    var pos = days.indexOf(dayT);
+    var daycount = pos; //counting what day it is 
+    const maxdisplay = daycount + 3
+    console.log(maxdisplay)
 
-    {
-        "day": "Thursday",
-        "max_temp":22.075, 
-        "min_temp":14.15, 
-        "wind_speed":8.889568364018134, 
-        "the_temp":27.665,
-        "rain": 4,
-        "weather_state_abbr":"hr"
-    },
+    var num = 0;
 
-    {
-        "day": "Friday",
-        "max_temp":30.075, 
-        "min_temp":20.15, 
-        "wind_speed":6.889568364018134, 
-        "the_temp":26.665,
-        "rain": 3,
-        "weather_state_abbr":"lc",
-    },
-    
-    {
-        "day":"Saturday",
-        "max_temp":29.075, 
-        "min_temp":19.15, 
-        "wind_speed":5.889568364018134, 
-        "the_temp":24.665,
-        "rain": 2,
-        "weather_state_abbr":"hr",
-    },
+    while (daycount < maxdisplay){
+        if (dayT == days[daycount]){
+            document.getElementById("the_temp").innerHTML = Math.round(weather.consolidated_weather[daycount].the_temp)
 
-    {
-        "day": "Sunday",
-        "max_temp":28.075, 
-        "min_temp":18.15, 
-        "wind_speed":4.889568364018134, 
-        "the_temp":25.665,
-        "rain": 1,
-        "weather_state_abbr":"s",
-    }
-];
+            while (daycount <= maxdisplay){
 
-var num = 0;
 
-while (daycount < maxdisplay){
-    if (dayT == days[daycount]){
-        while (daycount <= maxdisplay){
-            //var string = daycount.toString();
-            wimg = weather[daycount].weather_state_abbr
-            document.getElementById("img"+num).src = "https://www.metaweather.com/static/img/weather/"+wimg+".svg"
-            document.getElementById("day"+num).innerHTML = days[daycount]
-            document.getElementById(num+"H").innerHTML = Math.round(weather[daycount].max_temp)
-            document.getElementById(num+"L").innerHTML = Math.round(weather[daycount].min_temp)
-            document.getElementById(num+"R").innerHTML = "Wind: "+ Math.round(weather[daycount].wind_speed) +""+"km/h"
-            document.getElementById(num + "W").innerHTML = "Rain: "+ Math.round(weather[daycount].rain) +""+"%"
-            console.log("----------------------")
-            console.log(daycount)
-            console.log(num)
-            daycount++
-            num ++
+
+                //var string = daycount.toString();
+                const wimg = weather.consolidated_weather[daycount].weather_state_abbr
+                document.getElementById("img"+num).src = "https://www.metaweather.com/static/img/weather/"+wimg+".svg"
+                document.getElementById("day"+num).innerHTML = days[daycount]
+                document.getElementById(num+"H").innerHTML = Math.round(weather.consolidated_weather[daycount].max_temp)
+                document.getElementById(num+"L").innerHTML = Math.round(weather.consolidated_weather[daycount].min_temp)
+                document.getElementById(num+"W").innerHTML = "Wind: "+ Math.round(weather.consolidated_weather[daycount].wind_speed) +""+"km/h"
+                console.log("----------------------")
+                console.log(daycount)
+                console.log(num)
+                daycount++
+                num ++
+            }
+        daycount ++
+        
         }
-    daycount ++
-    
     }
-}
+
+}, false);
